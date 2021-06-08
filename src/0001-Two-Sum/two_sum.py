de6bd4d0d181dd65c20915fed1a2f5e9d66c36b1
rainiezzyycc <43354799+rainiezzyycc@users.noreply.github.com>
2021-06-08 03:04:53 -0400

    Update two_sum.py

--- src/0001-Two-Sum/two_sum.py
+++ src/0001-Two-Sum/two_sum.py
@@ -1,3 +1,8 @@
+
+#Difference: if use enumerate() or not
+
+##Solution 1
+
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -19,3 +24,25 @@ if __name__ == '__main__':
     nums = [1, 2, 3]
     target = 3
     print(Solution().twoSum(nums, target))
+    
+    
+    
+       
+    
+# Solution 2  
+    
+class Solution:
+    def twoSums(self,nums,target):
+        nums_hash = {}
+        for i ,v in enumerate(nums):
+            dif = target - v
+            if dif in nums_hash:
+                return [nums_hash[dif],i]
+            nums_hash[v]=i
+        return []
+
+
+if __name__ == '__main__':
+nums = [1, 2, 3]
+target = 3
+print(Solution().twoSum(nums, target))
